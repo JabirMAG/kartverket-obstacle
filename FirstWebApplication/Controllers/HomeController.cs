3cf5dde5aad8d6370c4e46486dbe652c8224928f
Hamdi205 <hamdiad@uia.no>
2025-09-18 13:11:30 +0200

    endringer

--- FirstWebApplication/Controllers/HomeController.cs
+++ FirstWebApplication/Controllers/HomeController.cs
@@ -15,49 +15,24 @@ namespace FirstWebApplication.Controllers
         // { 
         // _logger = logger;
         // }
-        
+
         public HomeController(IConfiguration config, ILogger<HomeController> logger)
         {
             _connectionString = config.GetConnectionString("DefaultConnection");
             _logger = logger;
         }
 
-        
-        public async Task<IActionResult> Index()
-        {
-            string viewModel = "Connected successfuly to MariaDB!";
-            try
-            {
-                await using var conn = new MySqlConnection(_connectionString);
-                await conn.OpenAsync();
-                return View("Index", viewModel);
-            }
-            catch (Exception ex)
-            {
-                return Content("Failed to connect to MariaDB: " + ex.Message);
-            }
-        }
 
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+     
 
-        // Blir kalt etter at vi trykker på "Register Obstacle" knappen
-
-        [HttpGet]
-        public IActionResult Dataform()
+       public IActionResult Index()
         {
             return View();
         }
 
+      
 
-        // Blir kalt etter at vi trykker på "Submit" knappen på Dataform viewwt
-        [HttpPost]
-        public IActionResult Dataform(ObstacleData obstacledata)
-        {
-            return View("Overview", obstacledata);
-        }
+
 
 
         public IActionResult Privacy()
@@ -70,5 +45,7 @@ namespace FirstWebApplication.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
+       
 }
--- FirstWebApplication/Models/ObstacleData.cs
+++ FirstWebApplication/Models/ObstacleData.cs
@@ -1,9 +1,20 @@
-﻿namespace FirstWebApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace FirstWebApplication.Models
 {
     public class ObstacleData
     {
+        [Required(ErrorMessage = "Field is required")]
+        [MaxLength(100)]
         public string ObstacleName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Field is required")]
+        [Range (0, 200)]
         public double ObstacleHeight { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        [MaxLength(1000)]
         public string ObstacleDescription { get; set; } = string.Empty;
+
+        public string? GeomertyGeoJason { get; set; }
     }
 }
