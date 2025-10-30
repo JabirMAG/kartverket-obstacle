7f1b3b233e5f48a2d06ed32bfec4919313fe5652
hannafalang04 <hfalang@gmail.com>
2025-10-30 13:21:20 +0100

    følger video 58 for å lage en siden for registrering. er ikke ferdig enda

---
+++ FirstWebApplication/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using FirstWebApplication.Models;
+using FirstWebApplication.Models.ViewModel;
+using FirstWebApplication.NewFolder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
+using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+
+namespace FirstWebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;    
+        }
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerViewModel.Username,
+                Email = registerViewModel.Email,
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password); //lager bruker
+
+            if (identityResult.Succeeded)
+            {
+                //assign this user the "User" role
+                var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
+
+                if (roleIdentityResult.Succeeded)
+                {
+                    //show success notification
+                    return RedirectToAction("Register");
+                }
+            }
+            //show error notification
+            return View();
+        }
+    }
+}
+
+
+
+
---
+++ FirstWebApplication/Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,8 @@
+namespace FirstWebApplication.Models.ViewModel;
+
+public class RegisterViewModel
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
