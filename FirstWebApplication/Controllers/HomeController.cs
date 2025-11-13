c68a05732b8d1aee4731ed793119caa5a26719c4
eliasnahiry <esnahiry@uia.no>
2025-11-13 11:14:59 +0100

    Om Oss
    Fiksa om oss siden

--- FirstWebApplication/Controllers/HomeController.cs
+++ FirstWebApplication/Controllers/HomeController.cs
@@ -50,6 +50,11 @@ namespace FirstWebApplication.Controllers
             return View();
         }
 
+        public IActionResult OmOss()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
