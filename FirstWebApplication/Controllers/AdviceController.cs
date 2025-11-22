daf366d9236059e270a39ce081f3c75b8eb4103a
hannafalang04 <hfalang@gmail.com>
2025-11-22 12:44:38 +0100

    Fikset på hjemskejremn med piler. Laget tester for AdviceController.cs

--- FirstWebApplication/Controllers/AdviceController.cs
+++ FirstWebApplication/Controllers/AdviceController.cs
@@ -2,58 +2,62 @@
 using FirstWebApplication.Models.ViewModel;
 using FirstWebApplication.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
-using System.Reflection;
 
 namespace FirstWebApplication.Controllers
 {
     public class AdviceController : Controller
     {
         private readonly IAdviceRepository _adviceRepository;
-        public AdviceController(IAdviceRepository adviceRepository) {
-           
+
+        public AdviceController(IAdviceRepository adviceRepository)
+        {
             _adviceRepository = adviceRepository;
         }
 
+        // GET: Viser skjemaet
         [HttpGet]
-        /*
-        public async Task<ActionResult> FeedbackForm(Advice Feedback)
-        {   
-            return View();
-        }
-*/
         public IActionResult FeedbackForm()
         {
-            return View();
+            return View(new AdviceViewModel());
         }
-        // Behandler skjemaet når det sendes inn av brukeren (Post- forespørsel)
+
+        // POST: Behandler innsending av skjemaet
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> FeedbackForm(AdviceViewModel requestData)
+        public async Task<IActionResult> FeedbackForm(AdviceViewModel model)
         {
             if (!ModelState.IsValid)
             {
-                return View(requestData);
+                return View(model);
             }
-            
+
             Advice advice = new Advice
             {
-                adviceMessage = requestData.ViewadviceMessage,
-                Email = requestData.ViewEmail,
+                adviceMessage = model.ViewadviceMessage,
+                Email = model.ViewEmail
             };
 
             await _adviceRepository.AddAdvice(advice);
-            return RedirectToAction("ThankForm", new { advice.Email, advice.adviceMessage });
 
+            return RedirectToAction("ThankForm", new
+            {
+                email = advice.Email,
+                message = advice.adviceMessage
+            });
         }
 
-        // Viser en takk-side etter at tilbakemeldingen er sendt inn
+        // GET: Viser takk-siden
         [HttpGet]
-        public IActionResult ThankForm(Advice adviceForm)
+        public IActionResult ThankForm(string email, string message)
         {
-            ViewBag.adviceMessage = adviceForm.adviceMessage;
-            return View(adviceForm);
+            var model = new Advice
+            {
+                Email = email,
+                adviceMessage = message
+            };
+
+            return View(model);
         }
     }
+}
 
- }
