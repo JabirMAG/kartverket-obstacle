e0342f30f90cd45a0f7a9d25f9f055b328b8fef6
Hamdi205 <hamdiad@uia.no>
2025-12-04 14:36:00 +0100

    Legg til CSS-filer, migrations, og dokumentasjon - alle CSHTML-filer har nå egen CSS

---
+++ FirstWebApplication/Migrations/20251125113058_FixPendingChanges.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstWebApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ObstaclesData_OwnerUserId",
+                table: "ObstaclesData",
+                column: "OwnerUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ObstaclesData_AspNetUsers_OwnerUserId",
+                table: "ObstaclesData",
+                column: "OwnerUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ObstaclesData_AspNetUsers_OwnerUserId",
+                table: "ObstaclesData");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ObstaclesData_OwnerUserId",
+                table: "ObstaclesData");
+        }
+    }
+}
